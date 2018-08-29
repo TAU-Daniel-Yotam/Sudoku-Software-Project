f78a23fe2cb60eb2e23c7ccd58a176999f1df5ad
yotammanne <yotammanne@mail.tau.ac.il>
2018-08-29 20:04:37 +0300

    fixed some compilation errors

--- Parser.h
+++ Parser.h
@@ -1,16 +1,18 @@
 
 #ifndef SUDOKU_SOFTWARE_PROJECT_PARSER_H
 #define SUDOKU_SOFTWARE_PROJECT_PARSER_H
-#include "Exceptions.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
 typedef struct Command{
     int type,numArgs,saveToList;
     int* intArgs;
     char* strArg;
 } Command;
+
+#include "Exceptions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
 
 int getInput(char* command, int size);
 int parseCommand(Game* game, char*command, Command* parsedCommand);
